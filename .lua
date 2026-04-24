f98c863fb49082f15208a58dd6957143bcc4548d
Targetlua <kiciabot2@gmail.com>
2026-04-23 22:33:41 -0400

    Delete .lua

--- .lua
+++
@@ -1,2 +0,0 @@
-script_key = "KEY" -- Paste your key here or if the script is free put trial
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/dc900a923ee7c06977e82bb1f13023b7eb9dd95e8a8afae1d258a790a585aec5.lua"))()
